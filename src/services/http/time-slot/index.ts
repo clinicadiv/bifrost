cd578507f4dfb10f76607ebf947842b3f3fa1c46
Matheus Antunes <dev.mathsbrain@gmail.com>
2025-07-04 11:44:56 -0300

    hot fix

--- src/services/http/time-slot/index.ts
+++ src/services/http/time-slot/index.ts
@@ -1,5 +1,3 @@
 export * from "./check-availability";
-export * from "./confirm-multiple-reservations";
 export * from "./create-guest-reservation";
 export * from "./create-reservation";
-export * from "./link-reservation";
